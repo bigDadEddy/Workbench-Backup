7ce4c27675d3684907187a79abc92ba21c4db547
Adam Cole <dalekcole@gmail.com>
2018-05-14 11:34:02 -0700

    Add files via upload

---
+++ Vintage Computer Art/js/graphFun.js
@@ -0,0 +1,66 @@
+var canvas = document.getElementById('animationContainer');
+
+var ox = window.innerWidth/2;
+var oy = window.innerHeight/2;
+
+//var origin = createRect(ox, oy, 1, 1);
+//canvas.appendChild(origin);
+
+// VARIABLES TO ADJUST
+var speed = 1;
+var height = window.innerHeight/10;
+var waveLength = window.innerWidth/20; //this variable needs work
+
+var dDegrees = Math.max(1, 1);
+
+var oy1 = window.innerHeight * 2 / 3;
+var x1 = ox * 2;
+var y1 = oy1;
+
+var oy2 = window.innerHeight / 3;
+var x2 = ox * 2;
+var y2 = oy2;
+
+var oy3 = window.innerHeight / 4;
+var x3 = ox * 2;
+var y3 = oy3;
+
+var points = [];
+points[0] = createRect(ox, oy, 1, 1); //primes the array
+
+//begin animating
+animController();
+
+function anim() {
+    
+    //move the spawning particle
+    dDegrees += 1;
+    dDegrees = dDegrees % 360;
+  
+    y1 = oy1 + height * Math.sin(degToRad(dDegrees));
+    
+    var tempPoint1 = createRect(x1, y1, 1, 1);
+    points.unshift(tempPoint1);
+    canvas.appendChild(tempPoint1);
+ // console.log(y1);  
+  
+    y2 = oy2 + height * Math.sin(degToRad(dDegrees));
+    var tempPoint2 = createRect(x2, y2, 1, 1);
+    points.unshift(tempPoint2);
+    canvas.appendChild(tempPoint2);
+    // console.log(y2);
+    
+    y3 = oy3 + height * Math.sin(degToRad(dDegrees));
+    var tempPoint3 = createRect(x3, y3, 1, 1);
+    points.unshift(tempPoint3);
+    canvas.appendChild(tempPoint3)
+
+    
+    for (var i = 0; i < points.length; i++) {
+        points[i].style.left = parseInt(points[i].style.left, 10) - speed + "px";
+    }
+    if (parseInt(points[points.length - 1].style.left) <= 0) {
+        points.pop();
+        canvas.removeChild(canvas.firstChild);
+    }
+}
---
+++ Vintage Computer Art/js/main.js
@@ -0,0 +1,49 @@
+var canvas = document.getElementById('animationContainer');
+
+//origin in center of container
+var ox = window.innerWidth/2;
+var oy = window.innerHeight/2;
+
+var l = ox * 3 / 4; //length of bars
+    n = 360 / 72; //distance between bars
+    x = ox + l * Math.cos(degToRad(45)); //end points of line
+    y = oy + l * Math.sin(degToRad(45)); //
+    s = 0; //starting offset of where lines are drawn
+    ds = .8; //rate at which the offset changes
+    c = 0; //variable to determine color
+	cd = .05; //rate at which color changes
+	startAnim(anim, 100);
+	console.log(rgbToHex(255,10,69));
+function anim() {
+    //clear canvas
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+    //change starting location of lines
+    s += ds
+    //s = s % 360;
+	//change length of lines
+	//l = ox + (1/4 * ox * Math.cos(degToRad(s * 3)));
+    //change color of lines
+	c += cd;
+	//c = c % 100;	
+	var r = Math.round(lerp(0, 100, Math.sin(.3 * c)/2 + 1));
+	    g = Math.round(lerp(0, 155, Math.sin(.4 * c)/2 + 1));
+	    b = Math.round(lerp(100, 255, Math.sin(.5 * c)/2 + 1));
+	    color = rgbToHex(r, g, b);
+	    colorR = rgbToHex(255, 0, 0);
+	    colorB = rgbToHex(0, 0, 255);
+
+	
+    for (var i = s; i < 360 + s; i += n) {
+		//l = ox * 3 / 4 + ox * 1 / 4 * Math.sin(degToRad(.7 * i + s));
+        x = ox + l * Math.cos(degToRad(1 * i)); 
+        y = oy + l * Math.sin(degToRad(1 * i));
+		var temp = i % 2 == 0 ? colorR : colorB;
+		console.log(temp);
+        var line = createLine(ox, oy, x, y, temp);		 
+        canvas.appendChild(line);
+    }
+
+    
+}
